ea5557cf97366802cbd60debccc210b2a1b26313
Aneeta <aneetajohn@gmail.com>
2024-03-22 14:07:51 -0400

    Reading Output.json from Downloads location if file exists else from Resource folder

--- src/main/java/com/oasis/poc1/controller/CoreWebArcGisController.java
+++ src/main/java/com/oasis/poc1/controller/CoreWebArcGisController.java
@@ -9,6 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.oasis.poc1.entity.OutputJson;
 import com.oasis.poc1.service.CoreWebArcGisService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+/**************
+ * Class: CoreWebArcGisController 
+ * 
+ * Purpose: Rest controller class that sends information from Core Web to ArcGIS Enterprise.
+ *          Poc1 - BiDirectional Communication - Core Web to ArcGIS Enterprise
+ */
+
+@Tag(name = "Poc 1 - BiDirectional Communication", description = "CoreWeb App - ArcGIS Communucation")
 @RestController
 @RequestMapping("/oasis/poc1")
 public class CoreWebArcGisController {
@@ -16,6 +25,12 @@ public class CoreWebArcGisController {
 	@Autowired
 	CoreWebArcGisService service;
 	
+	/**************
+	 * Method: getCoreWebArcGisCommunication 
+	 * Purpose: This method will return updated UI form elements in the response
+	 * Input parameters: None
+	 * @return OutputJson
+	 */
 	@GetMapping("/getCoreWebArcGisCommunication")
 	public ResponseEntity<OutputJson[]> getCoreWebArcGisCommunication(){
 		ResponseEntity<OutputJson[]> responseEntity=service.getCoreWebArcGisCommunication();
--- src/main/java/com/oasis/poc1/service/CoreWebArcGisService.java
+++ src/main/java/com/oasis/poc1/service/CoreWebArcGisService.java
@@ -1,6 +1,10 @@
 package com.oasis.poc1.service;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Objects;
 
 import org.slf4j.Logger;
@@ -15,6 +19,13 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oasis.poc1.entity.OutputJson;
 
+/**************
+ * Class: CoreWebArcGisService 
+ * 
+ * Purpose: Acts as a service layer that sends information from Core Web to ArcGIS Enterprise.
+ *     Poc1 - BiDirectional Communication - Core Web to ArcGIS Enterprise
+ */
+
 @Service
 public class CoreWebArcGisService {
 	
@@ -23,15 +34,33 @@ public class CoreWebArcGisService {
 	
 	Logger logger = LoggerFactory.getLogger(CoreWebArcGisService.class);
 	
+	/**************
+	 * Method: getCoreWebArcGisCommunication 
+	 * Purpose: This method will check for the updated data file 
+	 *          and read its contents and send it back as response        
+	 * Input parameters: None
+	 * @return OutputJson[]
+	 */
 	public ResponseEntity<OutputJson[]> getCoreWebArcGisCommunication(){
-		logger.info("getCoreWebArcGisCommunication() begins");
+		logger.info("CoreWebArcGisService - getCoreWebArcGisCommunication() begins");
 		ResponseEntity<OutputJson[]> responseEntity=null;		
 		try {
-			Resource resource = resourceLoader.getResource("classpath:output.json");
-			String fileContent = new String(resource.getInputStream().readAllBytes());
+			String home = System.getProperty("user.home");
+			String filepath = home+"/Downloads/Output.json";
+			File file = new File(filepath);
+			String fileContent ="";
+			if(file.exists() && file.canRead()) {
+				//Reading Output.JSON file from Downloads path if file exists
+				 fileContent = new String(Files.readAllBytes(Paths.get(filepath)));				
+			}else {
+				//Reading Output.JSON file in Resource folder 
+				Resource resource = resourceLoader.getResource("classpath:output.json");
+				fileContent = new String(resource.getInputStream().readAllBytes());
+			}
 			if(Objects.nonNull(fileContent)&&!fileContent.isEmpty()) {
 				ObjectMapper objectMapper = new ObjectMapper();
 				OutputJson[] response = objectMapper.readValue(fileContent, OutputJson[].class);
+				logger.info("**** Response : "+ Arrays.toString(response) + "****");
 				responseEntity = ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(response);
 			}else {
 				responseEntity = ResponseEntity.noContent().build();
@@ -39,7 +68,7 @@ public class CoreWebArcGisService {
 		} catch (IOException e) {
 			logger.error("Error in reading JSON file: "+ e.getMessage());			
 		}
-		logger.info("getCoreWebArcGisCommunication() ends");
+		logger.info("CoreWebArcGisService - getCoreWebArcGisCommunication() ends");
 		return responseEntity;	
 	}
 }
